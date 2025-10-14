a3216060f1d4814beef16c182550bf49f0b18aa6
MathematicGuy <nhatthanhdinh482@gmail.com>
2025-10-14 22:48:22 +0700

    Sav

--- utils/write_answers.py
+++ utils/write_answers.py
@@ -1,3 +1,5 @@
+import os
+
 def write_answers_to_file(answers):
     with open('data/answer.md', 'w') as f:
         f.write("### TASK QA\n")
@@ -11,3 +13,31 @@ def write_answers_to_file(answers):
                 f.write(f"{num_correct},{answer}\n")
             else:
                 f.write(f"{num_correct},\"{answer}\"\n")
+
+
+def write_extract_to_file(extracted_folder):
+    file_names = [d for d in os.listdir(extracted_folder) if os.path.isdir(os.path.join(extracted_folder, d))]
+    print(file_names)
+    with open('temp_answer.md', 'w', encoding='utf-8') as f:
+        f.write('### TASK EXTRACT\n')
+
+    for i, file_name in enumerate(file_names):
+        MARKDOWN_DIR = os.path.join(extracted_folder, file_name)
+        print('MARKDOWN_DIR:', MARKDOWN_DIR)
+
+        with open(f'{MARKDOWN_DIR}/main.md', 'r', encoding='utf-8') as f:
+            content = f.read()
+        with open('temp_answer.md', 'a', encoding='utf-8') as f:
+            f.write(f'\n\n# {file_name[:6]}_{file_name[-3:]}\n{content}')
+
+    # Move content from temp_answer.md to answer.md
+    with open('temp_answer.md', 'r', encoding='utf-8') as temp_f:
+        temp_content = temp_f.read()
+    with open('../answer.md', 'w', encoding='utf-8') as f:
+        f.write(temp_content)
+    os.remove('temp_answer.md')
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    PATH = "../private-test-output"
+    write_extract_to_file(PATH)
